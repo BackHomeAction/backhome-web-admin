6f1459ab8464ff53ee5ffd38983835f0ca046a37
x-zhao-res <1025496505@qq.com>
2021-03-29 20:53:55 +0800

    feat:初始化图表

--- src/views/dashboard/Workplace.vue
+++ src/views/dashboard/Workplace.vue
@@ -127,7 +127,15 @@
           :xs="24">
           <a-card title="团队指数" style="margin-bottom: 24px" :bordered="false" :body-style="{padding: 0}">
             <div class="item-group">
-              暂时空
+              <v-chart :forceFit="true" height="500" :data="data" :padding="padding" :scale="scale">
+                <v-tooltip />
+                <v-axis :dataKey="axis1Opts.dataKey" :line="axis1Opts.line" :tickLine="axis1Opts.tickLine" :grid="axis1Opts.grid" />
+                <v-axis :dataKey="axis2Opts.dataKey" :line="axis2Opts.line" :tickLine="axis2Opts.tickLine" :grid="axis2Opts.grid" />
+                <v-legend dataKey="user" marker="circle" :offset="30" />
+                <v-coord type="polar" radius="0.8" />
+                <v-line position="item*score" color="user" :size="2" />
+                <v-point position="item*score" color="user" :size="4" shape="circle" />
+              </v-chart>
             </div>
           </a-card>
           <a-card :loading="loading3" title="今日活跃志愿者" :bordered="false">
@@ -156,6 +164,61 @@ import { adminUser, adminDymic } from '@/api/admin'
 import { getMissionListAll } from '@/api/mission'
 import { getVolunteerList, VolunteerFire } from '@/api/volunteerAdmin'
 import dayjs from '@/utils/dayjs'
+import Viser from 'viser-vue'
+const DataSet = require('@antv/data-set')
+
+const sourceData = [
+  { item: 'Design', a: 70, b: 30 },
+  { item: 'Development', a: 60, b: 70 },
+  { item: 'Marketing', a: 50, b: 60 },
+  { item: 'Users', a: 40, b: 50 },
+  { item: 'Test', a: 60, b: 70 },
+  { item: 'Language', a: 70, b: 50 },
+  { item: 'Technology', a: 50, b: 40 },
+  { item: 'Support', a: 30, b: 40 },
+  { item: 'Sales', a: 60, b: 40 },
+  { item: 'UX', a: 50, b: 60 }
+]
+const dv = new DataSet.View().source(sourceData)
+dv.transform({
+  type: 'fold',
+  fields: ['a', 'b'],
+  key: 'user',
+  value: 'score'
+})
+const data = dv.rows
+
+const scale = [{
+  dataKey: 'score',
+  min: 0,
+  max: 80
+}]
+
+// eslint-disable-next-line no-unused-vars
+const axis1Opts = {
+  dataKey: 'item',
+  line: null,
+  tickLine: null,
+  grid: {
+    lineStyle: {
+      lineDash: null
+    },
+    hideFirstLine: false
+  }
+}
+// eslint-disable-next-line no-unused-vars
+const axis2Opts = {
+  dataKey: 'score',
+  line: null,
+  tickLine: null,
+  grid: {
+    type: 'polygon',
+    lineStyle: {
+      lineDash: null
+    }
+  }
+}
+
 export default {
   mounted () {
     adminUser().then(res => {
@@ -169,7 +232,7 @@ export default {
   },
   name: 'Workplace',
   components: {
-    PageHeaderWrapper, dayjs
+    PageHeaderWrapper, dayjs, Viser
   },
   filters: {
     timeget: function (val) {
@@ -178,6 +241,11 @@ export default {
   },
   data () {
     return {
+      scale,
+      axis1Opts,
+      axis2Opts,
+      data,
+      padding: [20, 20, 20, 20],
       timeFix: timeFix(),
       loading1: true,
       loading2: true,
