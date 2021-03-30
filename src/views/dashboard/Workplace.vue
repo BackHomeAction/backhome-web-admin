f15ac591e90d9ab27939da0810e15db53acca8b6
x-zhao-res <1025496505@qq.com>
2021-03-30 19:22:24 +0800

    feat:创建了图表样例

--- src/views/dashboard/Workplace.vue
+++ src/views/dashboard/Workplace.vue
@@ -126,16 +126,8 @@
           :sm="24"
           :xs="24">
           <a-card title="团队指数" style="margin-bottom: 24px" :bordered="false" :body-style="{padding: 0}">
-            <div class="item-group">
-              <v-chart :forceFit="true" height="500" :data="data" :padding="padding" :scale="scale">
-                <v-tooltip />
-                <v-axis :dataKey="axis1Opts.dataKey" :line="axis1Opts.line" :tickLine="axis1Opts.tickLine" :grid="axis1Opts.grid" />
-                <v-axis :dataKey="axis2Opts.dataKey" :line="axis2Opts.line" :tickLine="axis2Opts.tickLine" :grid="axis2Opts.grid" />
-                <v-legend dataKey="user" marker="circle" :offset="30" />
-                <v-coord type="polar" radius="0.8" />
-                <v-line position="item*score" color="user" :size="2" />
-                <v-point position="item*score" color="user" :size="4" shape="circle" />
-              </v-chart>
+            <div>
+              <charts></charts>
             </div>
           </a-card>
           <a-card :loading="loading3" title="今日活跃志愿者" :bordered="false">
@@ -164,60 +156,7 @@ import { adminUser, adminDymic } from '@/api/admin'
 import { getMissionListAll } from '@/api/mission'
 import { getVolunteerList, VolunteerFire } from '@/api/volunteerAdmin'
 import dayjs from '@/utils/dayjs'
-import Viser from 'viser-vue'
-const DataSet = require('@antv/data-set')
-
-const sourceData = [
-  { item: 'Design', a: 70, b: 30 },
-  { item: 'Development', a: 60, b: 70 },
-  { item: 'Marketing', a: 50, b: 60 },
-  { item: 'Users', a: 40, b: 50 },
-  { item: 'Test', a: 60, b: 70 },
-  { item: 'Language', a: 70, b: 50 },
-  { item: 'Technology', a: 50, b: 40 },
-  { item: 'Support', a: 30, b: 40 },
-  { item: 'Sales', a: 60, b: 40 },
-  { item: 'UX', a: 50, b: 60 }
-]
-const dv = new DataSet.View().source(sourceData)
-dv.transform({
-  type: 'fold',
-  fields: ['a', 'b'],
-  key: 'user',
-  value: 'score'
-})
-const data = dv.rows
-
-const scale = [{
-  dataKey: 'score',
-  min: 0,
-  max: 80
-}]
-
-// eslint-disable-next-line no-unused-vars
-const axis1Opts = {
-  dataKey: 'item',
-  line: null,
-  tickLine: null,
-  grid: {
-    lineStyle: {
-      lineDash: null
-    },
-    hideFirstLine: false
-  }
-}
-// eslint-disable-next-line no-unused-vars
-const axis2Opts = {
-  dataKey: 'score',
-  line: null,
-  tickLine: null,
-  grid: {
-    type: 'polygon',
-    lineStyle: {
-      lineDash: null
-    }
-  }
-}
+import charts from '@/views/dashboard/component/charts'
 
 export default {
   mounted () {
@@ -232,7 +171,7 @@ export default {
   },
   name: 'Workplace',
   components: {
-    PageHeaderWrapper, dayjs, Viser
+    PageHeaderWrapper, dayjs, charts
   },
   filters: {
     timeget: function (val) {
@@ -241,10 +180,6 @@ export default {
   },
   data () {
     return {
-      scale,
-      axis1Opts,
-      axis2Opts,
-      data,
       padding: [20, 20, 20, 20],
       timeFix: timeFix(),
       loading1: true,
---
+++ src/views/dashboard/component/charts.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-chart
+      :forceFit="true"
+      height="400"
+      :data="datas"
+      :padding="padding"
+      :scale="scale">
+      <v-tooltip />
+      <v-axis :dataKey="axis1Opts.dataKey" :line="axis1Opts.line" :tickLine="axis1Opts.tickLine" :grid="axis1Opts.grid" />
+      <v-axis :dataKey="axis2Opts.dataKey" :line="axis2Opts.line" :tickLine="axis2Opts.tickLine" :grid="axis2Opts.grid" />
+      <v-legend dataKey="user" marker="circle" :offset="30" />
+      <v-coord type="polar" radius="0.8" />
+      <v-line position="item*score" color="user" :size="2" />
+      <v-point position="item*score" color="user" :size="4" shape="circle" />
+    </v-chart>
+  </div>
+</template>
+
+<script>
+import Viser from 'viser-vue'
+const axis1Opts = {
+  dataKey: 'item',
+  line: null,
+  tickLine: null,
+  grid: {
+    lineStyle: {
+      lineDash: null
+    },
+    hideFirstLine: false
+  }
+}
+const axis2Opts = {
+  dataKey: 'score',
+  line: null,
+  tickLine: null,
+  grid: {
+    type: 'polygon',
+    lineStyle: {
+      lineDash: null
+    }
+  }
+}
+const DataSet = require('@antv/data-set')
+export default {
+  mounted () {
+    const dv = new DataSet.View().source(this.dataSource)
+    dv.transform({
+      type: 'fold',
+      fields: ['全国', '全省', '全区'],
+      key: 'user',
+      value: 'score'
+    })
+    this.datas = dv.rows
+  },
+  name: 'Charts',
+  data () {
+    return {
+      datas: null,
+      dataSource: [
+        { item: '结案率', '全国': 70, '全省': 46, '全区': 33 },
+        { item: '活跃度', '全国': 45, '全省': 34, '全区': 46 },
+        { item: '注册数', '全国': 62, '全省': 58, '全区': 73 },
+        { item: '结案数', '全国': 58, '全省': 46, '全区': 61 },
+        { item: '结案时间', '全国': 41, '全省': 12, '全区': 28 }
+      ],
+      axis2Opts,
+      axis1Opts,
+      padding: [30, 30, 30, 30],
+      scale: [{
+        dataKey: 'score',
+        min: 20,
+        max: 80
+      }]
+    }
+  },
+  components: {
+    Viser
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
