bf6937c45e179b22e9fb405fe8ba7f82d4d79805
x-zhao-res <1025496505@qq.com>
2021-04-03 01:26:25 +0800

    fix:修复了chart的tooltip不显示问题

--- src/views/dashboard/component/charts.vue
+++ src/views/dashboard/component/charts.vue
@@ -12,22 +12,22 @@
       <v-coord type="polar" radius="0.8" />
       <v-line position="item*score" color="user" :size="2" />
       <v-point position="item*score" color="user" :size="4" shape="circle" />
-      <v-tooltip :onChange="takeDown" />
+      <v-tooltip :onChange="takeDown"/>
     </v-chart>
     <div class="chartpoint">
       <div style="width: 100px">
         <div class="center" ><span><a-badge color="blue" text="全国" /></span></div>
-        <div class="center" ><div>{{ (arrayFather[0] ? arrayFather[0] : ' - - ') + (arrayFather[3] ? arrayFather[3] : '' ) + (arrayFather[4] ? arrayFather[4] : '') }}</div></div>
+        <div class="center" ><div>{{ (tipone ? tipone : ' - - ') + (twotype ? twotype : '' ) + (onetype ? onetype : '') }}</div></div>
       </div>
       <a-divider type="vertical" style="height: 70px" v-if="showOther" />
       <div style="width: 100px" v-if="showOther">
         <div class="center" ><span><a-badge color="yellow" text="全省" /></span></div>
-        <div class="center" ><div>{{ (arrayFather[1] ? arrayFather[1] : ' - - ') + (arrayFather[3] ? arrayFather[3] : '') }}</div></div>
+        <div class="center" ><div>{{ (tiptwo ? tiptwo : ' - - ') + (twotype ? twotype : '') }}</div></div>
       </div>
       <a-divider type="vertical" style="height: 70px" v-if="showOther" />
       <div style="width: 100px" v-if="showOther" >
         <div class="center" ><span><a-badge color="green" text="全区" /></span></div>
-        <div class="center" ><div>{{ (arrayFather[2] ? arrayFather[2] : ' - - ') + (arrayFather[3] ? arrayFather[3] : '') }}</div></div>
+        <div class="center" ><div>{{ (tipthree ? tipthree : ' - - ') + (twotype ? twotype : '') }}</div></div>
       </div>
     </div>
   </div>
@@ -71,12 +71,16 @@ export default {
     return {
       datas: null,
       axis2Opts,
-      arrayFather: [0, 0, 0],
       axis1Opts,
       dataSource: null,
       digest: null,
       showOther: false,
       region: [],
+      tipone: 0,
+      tiptwo: 0,
+      tipthree: 0,
+      onetype: '',
+      twotype: '',
       padding: [10, 10, 10, 10],
       scale: [{
         dataKey: 'score',
@@ -85,46 +89,8 @@ export default {
       }],
       userData: [],
       takeDown: (ev, chart) => {
-        if (ev.items.length === 1) {
-          this.arrayFather = []
-          this.arrayFather[0] = ev.items[0].value
-          this.$store.state.data.titles = ev.items[0].title
-          if (ev.items[0].title === '结案数' || ev.items[0].title === '注册数') {
-            this.arrayFather[4] = '个'
-          }
-          if (ev.items[0].title === '结案时间') {
-            this.arrayFather[4] = '天'
-          }
-          if (ev.items[0].title === '活跃度') {
-            this.arrayFather[4] = ' 人次'
-          }
-          if (ev.items[0].title === '结案率') {
-            this.arrayFather[4] = '/%'
-            this.arrayFather[0] = ev.items[0].value
-          }
-        } else {
-          this.$store.state.data.titles = null
-          this.$store.state.data.titles = ev.items[0].title
-          this.arrayFather = []
-          this.arrayFather[0] = ev.items[0].value
-          this.arrayFather[1] = ev.items[1].value
-          this.arrayFather[2] = ev.items[2].value
-          if (ev.items[0].title === '结案数' || ev.items[0].title === '注册数') {
-            this.arrayFather[3] = '个'
-          }
-          if (ev.items[0].title === '结案率') {
-            this.arrayFather[3] = '/%'
-            this.arrayFather[0] = ev.items[0].value
-            this.arrayFather[1] = ev.items[1].value
-            this.arrayFather[2] = ev.items[2].value
-          }
-          if (ev.items[0].title === '结案时间') {
-            this.arrayFather[3] = '天'
-          }
-          if (ev.items[0].title === '活跃度') {
-            this.arrayFather[3] = ' 人次'
-          }
-        }
+        this.$store.state.data.ev = ev
+        this.flash()
       }
     }
   },
@@ -171,11 +137,36 @@ export default {
           }).then(res => {
             this.$store.state.data.dataSource = res.data
             this.dataSource = [
-              { item: '结案率', '全国': Math.ceil(this.$store.state.data.dataSource.country.finishCaseRate * 100), '全省': Math.ceil(this.$store.state.data.dataSource.province.finishCaseRate * 100), '全区': Math.ceil(this.$store.state.data.dataSource.district.finishCaseRate * 100) },
-              { item: '活跃度', '全国': this.$store.state.data.dataSource.country.liveNess, '全省': this.$store.state.data.dataSource.province.liveNess, '全区': this.$store.state.data.dataSource.district.liveNess },
-              { item: '注册数', '全国': this.$store.state.data.dataSource.country.registerNum, '全省': this.$store.state.data.dataSource.province.registerNum, '全区': this.$store.state.data.dataSource.district.registerNum },
-              { item: '结案数', '全国': this.$store.state.data.dataSource.country.finishCaseNum, '全省': this.$store.state.data.dataSource.province.finishCaseNum, '全区': this.$store.state.data.dataSource.district.finishCaseNum },
-              { item: '结案时间', '全国': (this.$store.state.data.dataSource.country.finishCaseTime / 24).toFixed(2), '全省': (this.$store.state.data.dataSource.province.finishCaseTime / 24).toFixed(2), '全区': (this.$store.state.data.dataSource.district.finishCaseTime / 24).toFixed(2) }
+              {
+                item: '结案率',
+                '全国': Math.ceil(this.$store.state.data.dataSource.country.finishCaseRate * 100),
+                '全省': Math.ceil(this.$store.state.data.dataSource.province.finishCaseRate * 100),
+                '全区': Math.ceil(this.$store.state.data.dataSource.district.finishCaseRate * 100)
+              },
+              {
+                item: '活跃度',
+                '全国': this.$store.state.data.dataSource.country.liveNess,
+                '全省': this.$store.state.data.dataSource.province.liveNess,
+                '全区': this.$store.state.data.dataSource.district.liveNess
+              },
+              {
+                item: '注册数',
+                '全国': this.$store.state.data.dataSource.country.registerNum,
+                '全省': this.$store.state.data.dataSource.province.registerNum,
+                '全区': this.$store.state.data.dataSource.district.registerNum
+              },
+              {
+                item: '结案数',
+                '全国': this.$store.state.data.dataSource.country.finishCaseNum,
+                '全省': this.$store.state.data.dataSource.province.finishCaseNum,
+                '全区': this.$store.state.data.dataSource.district.finishCaseNum
+              },
+              {
+                item: '结案时间',
+                '全国': (this.$store.state.data.dataSource.country.finishCaseTime / 24).toFixed(2),
+                '全省': (this.$store.state.data.dataSource.province.finishCaseTime / 24).toFixed(2),
+                '全区': (this.$store.state.data.dataSource.district.finishCaseTime / 24).toFixed(2)
+              }
             ]
             console.log(this.dataSource)
             const DataSet = require('@antv/data-set')
@@ -191,6 +182,48 @@ export default {
           })
         }
       })
+    },
+    flash: function () {
+      const ev = this.$store.state.data.ev
+      if (ev.items.length === 1) {
+        this.tipone = ev.items[0].value
+        this.$store.state.data.titles = ev.items[0].title
+        if (ev.items[0].title === '结案数' || ev.items[0].title === '注册数') {
+          this.onetype = '个'
+        }
+        if (ev.items[0].title === '结案时间') {
+          this.onetype = '天'
+        }
+        if (ev.items[0].title === '活跃度') {
+          this.onetype = ' 人次'
+        }
+        if (ev.items[0].title === '结案率') {
+          this.onetype = '/%'
+          this.tipone = ev.items[0].value
+        }
+      } else {
+        this.$store.state.data.titles = null
+        this.$store.state.data.titles = ev.items[0].title
+        this.tipone = ev.items[0].value
+        this.tiptwo = ev.items[1].value
+        this.tipthree = ev.items[2].value
+        if (ev.items[0].title === '结案数' || ev.items[0].title === '注册数') {
+          this.twotype = '个'
+        }
+        if (ev.items[0].title === '结案率') {
+          this.twotype = '/%'
+          this.tipone = ev.items[0].value
+          this.tiptwo = ev.items[1].value
+          this.tipthree = ev.items[2].value
+        }
+        if (ev.items[0].title === '结案时间') {
+          this.twotype = '天'
+        }
+        if (ev.items[0].title === '活跃度') {
+          this.twotype = ' 人次'
+        }
+        this.onetype = ''
+      }
     }
   }
 }
